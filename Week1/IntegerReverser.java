ae46052137340f7b1bc401446a2076a57f6d1a95
askioas <askioas@hotmail.com>
2026-04-27 08:19:33 +0000

    Made IntegerReverser actually work now.

--- Week1/IntegerReverser.java
+++ Week1/IntegerReverser.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class IntegerReverser {
     public static void main(String[] args){
-     // Scanner to recieve First integer
+     // Scanner to recieve first integer
      Scanner firstinteger = new Scanner(System.in);   
      System.out.print("Input first Integer: ");
      int FirstInteger = Integer.parseInt(firstinteger.nextLine());
-     // Scanner to recieve Second integer
+     // Scanner to recieve second integer
      Scanner secondinteger = new Scanner(System.in);
      System.out.print("Input second Integer: ");
      int SecondInteger = Integer.parseInt(secondinteger.nextLine());
-     // Outputs the Integer Normally then Reversed
-    System.out.println(FirstInteger+" and "+SecondInteger+" reversed are "+IntegerReverser(FirstInteger))
+     // Outputs the Integer Normally then in reversed order
+    System.out.println(FirstInteger + " and " + SecondInteger + " in reverse order is: " + SecondInteger + " " + FirstInteger);
     }
 }
